70977a74e30e7135d6c9d982fb8ab8658fa3d8fd
Abhi Gupta <abhi.gupta@columbia.edu>
2021-05-22 21:44:06 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export SCIPOPTDIR='/opt/SCIP6/scip'
+
+cd scip/soplex-4.0.1
+rm -rf build
+mkdir build
+cmake -S . -B build -DCMAKE_INSTALL_PREFIX=$SCIPOPTDIR
+make -C ./build -j 4
+make -C ./build install
+cd ..
+
+cd scip-6.0.1/
+rm -rf build
+mkdir build
+cmake -S . -B build -DSOPLEX_DIR=$SCIPOPTDIR -DCMAKE_INSTALL_PREFIX=$SCIPOPTDIR
+make -C ./build -j 4
+make -C ./build install
+cd ..
