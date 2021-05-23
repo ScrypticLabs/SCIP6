7b5d75e3b7498dba21c3c80dfe68485a59267a1d
Abhi Gupta <abhi.gupta@columbia.edu>
2021-05-22 22:30:31 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-pwd
-
 sudo apt install -y cmake
 
 export SCIPOPTDIR='/opt/SCIP6/scip'
 
 cd scip/soplex-4.0.1
-rm -rf build
-mkdir build
 cmake -S . -B build -DCMAKE_INSTALL_PREFIX=$SCIPOPTDIR
 make -C ./build -j 4
 make -C ./build install
 cd ..
 
 cd scip-6.0.1/
-rm -rf build
-mkdir build
 cmake -S . -B build -DSOPLEX_DIR=$SCIPOPTDIR -DCMAKE_INSTALL_PREFIX=$SCIPOPTDIR
 make -C ./build -j 4
 make -C ./build install
